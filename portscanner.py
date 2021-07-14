35cfd73ba58dce9a648025a80426d6822eaa2c10
samlaubscher <hakaewt@protonmail.com>
2021-07-14 14:49:36 +0100

    Add title ASCII text, and finish functionality

--- portscanner.py
+++ portscanner.py
@@ -1,9 +1,9 @@
 import socket #  library allows script to communicate with other machines using TCP & UDP protocols
-import termcolor #  used just to print statements in different colors
 
 
 # concat function
 def scan(target, ports):
+    print('\n' + 'Starting Scan For ' + str(target))
     for port in range(1, ports):
         scan_port(target, port)
 
@@ -16,13 +16,22 @@ def scan_port(ipaddress, port):
         print("[+] Port Opened " + str(port))
         sock.close()
     except:
-        print("[-] Port Closed " + str(port))
+        pass
+
+
+print('''
+.__            . __      .     .__        ,    __.                  
+[__) _ .    , _|/  ` _  _| _   [__) _ ._.-+-  (__  _. _.._ ._  _ ._.
+|  \(/, \/\/ (_]\__.(_)(_](/,  |   (_)[   |   .__)(_.(_][ )[ )(/,[  
+                                                                    
+'''
+)
 
 # target IP addresses, allowing for multiple entries
 targets = input("[*] Enter Targets To Scan (seperate with ,): ")
 
-# number of ports from 1 to input value
-ports = input("[*] Enter How Many Ports You Want To Scan: ")
+# number of ports from 1 to input value, added one additional port to make sure last port is also scanned
+ports = 1 + int(input("[*] Enter How Many Ports You Want To Scan: "))
 
 # split the IP addresses and strip spaces if multiple
 if ',' in targets:
